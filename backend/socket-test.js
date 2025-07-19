9d30e0c2f886092be1044eb42be05ee558eaae6b
Mourtalla Diop <161357178+Mourtish@users.noreply.github.com>
2025-07-19 07:29:34 +0000

    Fix database connection and Docker setup

---
+++ backend/socket-test.js
@@ -0,0 +1,8 @@
+const io = require('socket.io-client');
+const socket = io('http://localhost:3001');
+socket.on('connect', () => {
+  console.log('Connected to Socket.IO server:', socket.id);
+});
+socket.on('disconnect', () => {
+  console.log('Disconnected from Socket.IO server');
+});
